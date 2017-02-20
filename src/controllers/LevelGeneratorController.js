5b46b27f5c07c1670a44b5a1408734dcbf353954
Karl Lundfall <karl@bizboard.nl>
2017-02-20 22:55:41 +0100

    Added built-in famous patching

--- src/controllers/LevelGeneratorController.js
+++ src/controllers/LevelGeneratorController.js
@@ -1,10 +1,23 @@
+import LayoutNode                   from 'famous-flex/LayoutNode.js';
+
 import {Controller}                 from 'arva-js/core/Controller.js';
 import {LevelGenerator}             from '../logic/LevelGenerator.js';
 
 export class LevelGeneratorController extends Controller {
 
-    CheckCollisions(){
-        return LevelGenerator.generateCollisionGraph();
+    CheckCollisions(visualFeedback = false) {
+        if (visualFeedback) {
+            /* Patch to make it work */
+            LayoutNode.prototype.getSpec = function() {
+                this._specModified = false;
+                /* This line is removed from the original, to make far apart updates possible without removing the node */
+                // this._spec.removed = !this._invalidated;
+                return this._spec;
+            };
+            return LevelGenerator.generateCollisionGraphVisualFeedback();
+        } else {
+            return LevelGenerator.generateCollisionGraph();
+        }
     }
 
     FindLevels() {
